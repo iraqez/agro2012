7ebfe4470db30544c85aa8207250c11072dffc9b
iraqez <vladimir.gumenyuk@agro2012.com.ua>
2016-10-22 14:39:12 +0300

    321

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from main.views import MainView, AboutView
+from main.views import MainView, AboutView, NewsView
 
 urlpatterns = [
     url(r'^$', MainView.as_view(), name='main'),
     url(r'^about/$', AboutView.as_view(), name='about'),
+    url(r'^news/$', NewsView.as_view(), name='news'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ class MainView(generic.TemplateView):
 
 class AboutView(generic.TemplateView):
     template_name = 'main/about.html'
+
+class NewsView(generic.TemplateView):
+    template_name = 'main/news.html'
