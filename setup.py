59f2db2741639875b57277ffd42c7a8a6e0480ff
iraqez <vladimir.gumenyuk@gmail.com>
2016-10-24 17:10:05 +0300

    12333

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='agroholding2012',
+    version='',
+    packages=['main', 'main.migrations', 'office', 'office.migrations', 'employees', 'employees.migrations',
+              'agroholding2012'],
+    url='',
+    license='',
+    author='v.gumenyuk',
+    author_email='',
+    description=''
+)
