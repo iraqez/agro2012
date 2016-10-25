003316d2f525999d411a2d5138b8ef20a7d1de11
Iraqez <vladimir.gumenyuk@gmail.com>
2016-10-25 22:40:40 +0300

    112

--- news/models.py
+++ news/models.py
@@ -1,13 +1,17 @@
+#http://cowo.ks.ua/10-5.html
+
 from django.db import models
 from datetime import datetime
 from django_resized import ResizedImageField
 from sorl.thumbnail import get_thumbnail
 
 class New(models.Model):
-    img = ResizedImageField(size=[850, 850], upload_to='news', verbose_name='Фото')
+    image = models.ImageField(size=[850, 850], upload_to='news', verbose_name='Фото')
     title = models.CharField(max_length=150, unique_for_date='posted', verbose_name='Заголовок')
     content = models.TextField(verbose_name='Зміст')
     posted = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name='Опублікована')
+
+
 
     def __str__(self):
         return self.title
