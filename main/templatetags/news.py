dcc908e40b7fbf3561433b4028fecafd9cb2bd91
iraqez <vladimir.gumenyuk@gmail.com>
2016-11-07 09:57:40 +0200

    12333

---
+++ main/templatetags/news.py
@@ -0,0 +1,9 @@
+from django import template
+from news.models import Tag, Category, New # импортируем модели
+# экземпляр класса, в котором все наши теги будут зарегистрированы
+register = template.Library()
+# регистрируем наш тег, который будет выводить шаблон right_sidebar.html
+@register.inclusion_tag("main/main_news.html")
+def show_news():
+    new_posts = New.objects.values('id','title', 'image', 'posted', 'content').order_by("-posted")[:4]
+    return {'new_posts': new_posts}
