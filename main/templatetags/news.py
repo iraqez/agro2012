a6aaf03fe38be16512b23e3551678197fdd0a37f
iraqez <vladimir.gumenyuk@gmail.com>
2016-11-07 11:44:39 +0200

    12333

--- main/templatetags/news.py
+++ main/templatetags/news.py
@@ -5,5 +5,5 @@ register = template.Library()
 # регистрируем наш тег, который будет выводить шаблон right_sidebar.html
 @register.inclusion_tag("main/main_news.html")
 def show_news():
-    new_posts = New.objects.values('id','title', 'image', 'posted', 'content').order_by("-posted")[:4]
+    new_posts = New.objects.values('id','title', 'image', 'posted', 'content', 'slug').order_by("-posted")[:4]
     return {'new_posts': new_posts}
