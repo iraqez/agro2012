3b29663ee64edafaad8969497328a41eb27977c8
Iraqez <vladimir.gumenyuk@gmail.com>
2016-11-29 20:13:57 +0200

    321

--- agro2012/dev_settings.py
+++ agro2012/dev_settings.py
@@ -1,19 +1,19 @@
 from .settings import *
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'dev-agro2012',
-    'USER': 'postgres',
-    'PASSWORD': 'workfree',
-    'HOST': 'localhost', # Set to empty string for localhost.
-    'PORT': '5432', # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': 'dev-agro2012',
+#     'USER': 'postgres',
+#     'PASSWORD': 'workfree',
+#     'HOST': 'localhost', # Set to empty string for localhost.
+#     'PORT': '5432', # Set to empty string for default.
+#     }
+# }
