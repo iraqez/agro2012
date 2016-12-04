199481636a45b9212a10483d209c82c785cee786
Iraqez <vladimir.gumenyuk@gmail.com>
2016-12-04 23:16:03 +0200

    321

--- agro2012/urls.py
+++ agro2012/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
  #   url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', admin.site.urls),
     url(r'^tiny_mce/', include('tinymce.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='agro2012/static/images_agroholding/favicon.ico')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
