6c4826d67c781e36740612299eef4df2afa46343
iraqez <vladimir.gumenyuk@gmail.com>
2016-12-13 10:28:40 +0200

    11111

--- news/urls.py
+++ news/urls.py
@@ -3,11 +3,5 @@ from news.views import NewsListView, NewsDetailView, category, tag
 
 urlpatterns = [
     url(r'^$', NewsListView.as_view(), name='news'),
-  #  url(r'^$', CategoryListView.as_view(), name='news'),
     url(r'^(?P<slug>[\w-]+)/$', NewsDetailView.as_view()),
-  #  url(r'^category/(?P<slug>\d+)$', 'news.views.category'),
-   #  url(r'^test/$', CategoryListView.as_view()),
-    # выборку будем делать по slug, представление - category
- #   url(r'^(?P<slug>[\w-]+)/$', 'category'),
- #   url(r'^tag/(?P<slug>[\w-]+)/$', 'tag'),  # тут тоже самое
 ]
